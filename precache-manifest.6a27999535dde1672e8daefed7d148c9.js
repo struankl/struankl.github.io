cb5bc5a1e24a7289a0d3046b5261da03d74c9336
Struan Kerr-Liddell <struan@grangesoftwaresolutions>
2018-11-01 08:18:28 +0000

    Public facing hosted simple stock market tracker

---
+++ precache-manifest.6a27999535dde1672e8daefed7d148c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "95ebdf427123507ca150",
+    "url": "/static/js/main.95ebdf42.chunk.js"
+  },
+  {
+    "revision": "edac6092135ffd964c8d",
+    "url": "/static/js/1.edac6092.chunk.js"
+  },
+  {
+    "revision": "95ebdf427123507ca150",
+    "url": "/static/css/main.aa97fc4d.chunk.css"
+  },
+  {
+    "revision": "8399fec13f9012288dec27533a96d034",
+    "url": "/index.html"
+  }
+];
